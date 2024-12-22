77b9c34d454ef8d83a317d4834f803c3203bba1f
codergb <betweent@126.com>
2024-12-22 16:11:52 +0800

    bug解决

--- gb-music-server/src/utils/alipay.js
+++ gb-music-server/src/utils/alipay.js
@@ -1,7 +1,7 @@
 const AlipaySdk = require('alipay-sdk').default; // 引入 SDK
 const { ALIPAY_PRIVATE_KEY, ALIPAY_APPLY_PUBLIC_KEY } = require('../app/config');
 const alipaySdk = new AlipaySdk({
-  appId: '902100013263733079198',
+  appId: '902100013263733077',
   signType: 'RSA2',
   gateway: 'https://openapi-sandbox.dl.alipaydev.com/gateway.do',
   alipayPublicKey: ALIPAY_APPLY_PUBLIC_KEY,
